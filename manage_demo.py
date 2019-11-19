ea1d64fa04ed7f38dc249f0bcba996ee15c809e8
sakurally128 <sakurally128@163.com>
2019-11-19 10:50:15 +0800

    更新备注

--- manage_demo.py
+++ manage_demo.py
@@ -8,6 +8,14 @@ migrate = Migrate(app,db)
 #1.要使用flask_migrate,必须先绑定db
 manager.add_command("db",MigrateCommand)
 
+'''
+python manage.py db init：这个命令不需要执行，因为已经初始化了迁移脚本的环境，这个命令只执行一次。
+
+python manage.py db migrate：这个命令需要执行，因为模型改变了。
+
+python manage.py db upgrade这个命令也需要执行，每次运行了migrate命令后，就记得要运行这个命令
+'''
+
 @manager.command
 def runserver():
     print(u"服务器跑起来了！！！")
